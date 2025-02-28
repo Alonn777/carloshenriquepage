8108da6ec64e832307c5215805b85fe7162082a1
Alonn777 <zilmavilasboas920@gmail.com>
2025-02-28 14:48:08 -0300

    First model Portfolio

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+let scroll = document.getElementById("project-scroll");
+let project = document.getElementById("project-section");
+let skill = document.getElementById("skill-section");
+let skillscroll = document.getElementById("skill-scroll");
+scroll
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    project.scrollIntoView({ behavior: "smooth" });
+  })
+
+  skillscroll.addEventListener("click", function (event) {
+    event.preventDefault();
+    skill.scrollIntoView({behavior:'smooth'})
+  });
